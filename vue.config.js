0a21a6c10677cf6ab1aeb2f3d1e127a7438975fe
sogrey <suzhaoheng2006@163.com>
2022-04-14 21:47:56 +0800

    💖 配置静态资源路径.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // 这里是配置上线读取当前目录，默认是根路径，如 /js, /css 等，具体根据项目来
     // baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-//     outputDir: 'dist',
-//     assetsDir: process.env.NODE_ENV === 'production' ? './' : '/',
+    publicPath: './',
+    //     outputDir: 'dist',
+    //     assetsDir: process.env.NODE_ENV === 'production' ? './' : '/',
 }

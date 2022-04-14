4671ca821155a323b4a3d0fb1d1ad45858f6d945
sogrey <suzhaoheng2006@163.com>
2022-04-15 01:10:31 +0800

    💖 review router.

--- vite.config.ts
+++ vite.config.ts
@@ -6,46 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: "./",
+    // root: "./",
     base: "./",
     mode:'development',// 'development'（开发模式），'production'（生产模式）
-    // publicDir:"./",
     plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    build: {
-        target: 'modules',
-        outDir: 'dist', // 指定输出路径
-        assetsDir: 'assets', // 指定生成静态资源的存放路径
-        minify: 'terser', // 混淆器,terser构建后文件体积更小
-        sourcemap: false,
-        // terserOptions: {
-        //     compress: {
-        //         drop_console: true, // 生产环境移除console
-        //         drop_debugger: true // 生产环境移除debugger
-        //     }
-        // },
-        // rollupOptions: {
-        //     treeshake: false,
-        //     output: {
-        //         manualChunks(id) {
-        //             if (id.includes('node_modules')) {
-        //                 return id.toString().split('node_modules/')[1].split('/')[0].toString()
-        //             }
-        //         }
-        //     }
-        // }
-    },
-    server: {
-        // open: true, // 是否在浏览器打开
-        // port: 8088, // 端口号
-        // host: 'local.xxx.com'
-    },
-    // 引入第三方配置
-    optimizeDeps: {
-        include: []
-    }
 })
